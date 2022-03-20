7a281f2c4ad3fee04ca3157c6bcba2024af59949
Zhuzi <85818541+Zhuzi-ouo@users.noreply.github.com>
2022-03-20 18:07:05 +0800

    Update

--- R6_Tracker.py
+++ R6_Tracker.py
@@ -1,14 +1,12 @@
-User=input ("Enter Your Rainbow Six Username:")
 import requests
 from bs4 import BeautifulSoup
-
-response=requests.get(f"https://r6.tracker.network/profile/pc/{User}")
-soup=BeautifulSoup(response.text,"html.parser")
-Overview=soup.find_all("div",class_="trn-defstat__value")
-r6usericon=soup.find_all("img")
-Usericon=r6usericon[1].get("src")
-print ("Wins",Overview[1].string)
-print ("WIN %",Overview[2].string)
-print ("KILLS",Overview[3].string)
-print ("KD",Overview[4].string)
-print (Usericon)
+def R6T(User):
+    response=requests.get(f"https://r6.tracker.network/profile/pc/{User}")
+    soup=BeautifulSoup(response.text,"html.parser")
+    Overview=soup.find_all("div",class_="trn-defstat__value")
+    r6usericon=soup.find_all("img")
+    R6T.Usericon=r6usericon[1].get("src")
+    R6T.Wins=Overview[1].string
+    R6T.WIN=Overview[2].string
+    R6T.KILLS=Overview[3].string
+    R6T.KD=Overview[4].string
